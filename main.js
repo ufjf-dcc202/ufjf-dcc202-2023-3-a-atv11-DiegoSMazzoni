74d279ec46ed10713f199bfbb684b83eca1a9550
Diego Mazzoni <diego.mazzoni@estudante.ufjf.br>
2023-11-29 20:33:40 -0300

    passa a funcao atualizar itens de lista

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 // main.js
+import { getLista, limparLista } from "./lista.js";
 
 // Passo 1 - Ache os elementos importantes
 const pEntrada = document.querySelector('#entrada');
@@ -6,3 +7,17 @@ const olSaida = document.querySelector('#saida');
 const btnAdicionar = document.querySelector('#adicionar');
 const btnLimpar = document.querySelector('#limpar');
 
+atualizarItensDeLista();
+limparLista();
+atualizarItensDeLista();
+
+function atualizarItensDeLista() {
+    olSaida.innerHTML = "";
+    const lista = getLista();
+    for (let i = 0; i < lista.length; i++){
+        const item = lista[i];
+        const li = document.createElement('li');
+        li.textContent = item;
+        olSaida.appendChild(li);
+    }
+}
